d6a916e3d297329dd2be09b1840a1c02dae3afa1
github username <josephtrettevik@gmail.com>
2019-08-20 16:34:06 -0700

    Created 4 Brewery and 3 User class instances in the seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 require
-Brewery.create()
+Brewery.create(name: "Brew1", location: "Seattle", url: "www.brew1.com")
+Brewery.create(name: "Brew2", location: "Seattle", url: "www.brew2.com")
+Brewery.create(name: "Brew3", location: "Seattle", url: "www.brew3.com")
+Brewery.create(name: "Brew4", location: "Seattle", url: "www.brew4.com")
+
+User.create(name: "Sarah")
+User.create(name: "John")
+User.create(name: "Brad")
+
