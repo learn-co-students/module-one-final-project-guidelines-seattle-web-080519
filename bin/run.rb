67aece6d82c2178c159e6636b55a445137889455
github username <josephtrettevik@gmail.com>
2019-08-21 15:39:58 -0700

    Debugged the Login/Create User Profile functionality.

--- bin/run.rb
+++ bin/run.rb
@@ -2,8 +2,28 @@ require_relative '../config/environment'
 require 'pry'
 
 welcome
-option = get_option_from_user
+
+option = get_login
+
+if option == '1'
+    current_user = get_user_profile
+elsif option == '2'
+    current_user = prompt_user_creation
+else
+    puts "Please enter a number that corresponds to one of the options above."   
+end
 
 binding.pry
+
+option = get_menu_option
+
+if option == '1'
+    
+elsif option == '2'
+    
+else
+    puts "Please enter a number that corresponds to one of the options above."   
+end
+
 
 puts "HELLO WORLD"
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,24 +1,42 @@
 require_relative '../config/environment'
 
 def welcome
-    p "Welcome to Breweries Go, where you can find and catch your favorite breweries."
+    puts "Welcome to Breweries Go, where you can find and catch your favorite breweries."
 end
   
-def get_option_from_user
+def get_login
   puts "Please select an option:"
-  puts "1. Create a User Profile."
-  puts "2. Search for a Breweries by city."
-  puts "3. See your list of Breweries."
-    
-  # puts "Please select an option:"
+  puts "1. Login with your Username."
+  puts "2. Create a User Profile."
 
-  # use gets to capture the user's input. This method should return that input, downcased.
-  option = gets.chomp.downcase
-  return option
+  # use gets to capture the user's input. 
+  option = gets.chomp
+  option
 end
 
 def prompt_user_creation
-  
-
+  User.create(name: get_username)
 end
+
+def get_user_profile
+  User.find_by(name: get_username)
+end
+
+def get_username
+  puts "Please a username and enter it into the console."
+  # binding.pry
+  username = gets.chomp
+  username
+end
+
+def get_menu_option
+  puts "Please select an option:"
+  puts "1. Search for a Breweries by city."
+  puts "2. See your list of Breweries."
+    
+  # use gets to capture the user's input.
+  option = gets.chomp
+  return option
+end
+
   
